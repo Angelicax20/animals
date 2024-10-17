f937a798fc2499b617ed2050f2bb85bbbf6f0a26
angelica <angelica.giraldo10699@gmail.com>
2024-10-17 15:41:07 -0500

    first commit

---
+++ animals/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+// src/pages/Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Bienvenido a Animal Aid</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ animals/src/pages/RegisterAnimal.tsx
@@ -0,0 +1,12 @@
+// src/pages/RegisterAnimal.tsx
+import React from 'react';
+
+const RegisterAnimal: React.FC = () => {
+  return (
+    <div>
+      <h1>Registrar un Animal</h1>
+    </div>
+  );
+}
+
+export default RegisterAnimal;
